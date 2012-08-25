f98dbe32f5cb83ece00c11ba1ebd86a0f5bf27c2
Michael Writhe <michael@writhem.com>
2012-08-24 23:04:14 -0600

    Advanced Admin page, and cleaned up order of models. no additional user functionality, admin only

--- cannen/admin.py
+++ cannen/admin.py
@@ -1,22 +1,44 @@
-from models import SongFile
-from models import UserSong
 from models import GlobalSong
 from models import GlobalSongRate
+from models import SongFile
 from models import SongFileScore
+from models import UserSong
 from models import UserProfile
 from django.contrib import admin
 
-class SongFileAdmin(admin.ModelAdmin):
-    fields = ['owner', 'file']
-    list_filter = ['owner']
-
-admin.site.register(SongFile, SongFileAdmin)
-admin.site.register(UserSong)
-admin.site.register(GlobalSong)
-
 class GlobalSongRateAdmin(admin.ModelAdmin):
     list_filter = ['subject','rater']
-
+    list_display = ['rater','rate','subject']
 admin.site.register(GlobalSongRate, GlobalSongRateAdmin)
-admin.site.register(SongFileScore)
-admin.site.register(UserProfile)
+
+class GlobalSongAdmin(admin.ModelAdmin):
+    list_filter = ['submitter']
+    list_display = ['is_playing','submitter','url']
+    list_display_links = ['url']
+admin.site.register(GlobalSong, GlobalSongAdmin)
+
+class SongFileScoreAdmin(admin.ModelAdmin):
+    ordering = ['url']
+    fields = ['url','score']
+    list_display = ['score','url']
+    list_display_links = ['url']
+admin.site.register(SongFileScore, SongFileScoreAdmin)
+
+class SongFileAdmin(admin.ModelAdmin):
+    ordering = ['file']
+    fields = ['owner', 'file']
+    list_display = ['owner','file']
+    list_filter = ['owner']
+    list_display_links = ['file']
+admin.site.register(SongFile, SongFileAdmin)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    ordering = ['user']
+    list_display = ['user','coinsEarned','coinsSpent']
+admin.site.register(UserProfile, UserProfileAdmin)
+
+class UserSongAdmin(admin.ModelAdmin):
+    ordering = ['owner','orderable_position']
+    list_display = ['orderable_position','owner','url']
+    list_display_links = ['url']
+admin.site.register(UserSong, UserSongAdmin)
